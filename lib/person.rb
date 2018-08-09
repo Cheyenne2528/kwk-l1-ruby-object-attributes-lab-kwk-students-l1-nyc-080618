fcce22ca519de50af936372718fc1f60945375c0
Cheyenne2528 <cheyennefun12@gmail.com>
2018-08-09 19:35:06 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,3 +3,19 @@ class Person
   @name = 
   @job = 
 end
+
+def name
+    @name
+  end 
+  
+  def name=(name)
+    @name = name 
+  end 
+  
+  def job
+    @job
+  end
+  
+  def job=(job)
+  @job = job
+end 
